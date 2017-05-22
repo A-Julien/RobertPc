6584b13414a4ef360024d4abec9c57326361311e
A-Julien <julien.alaimo@gmail.com>
2017-05-22 17:01:50 +0200

    -a
    ajout du generateur de code

---
+++ modele/classes/RobertGenerate.php
@@ -0,0 +1,23 @@
+<?php
+		$phpStart = "<?php\n";
+		$phpEnd = "?>";
+		$droit = "public";
+		$include = "\trequire_once('Generique.class.php')";
+
+		function($fileName,$className,$nbAttribut,$tabAttribut){
+			$classHeader = "\tclass ".$className."extends Generique {\n";
+			$classFooter = "\t}\n";
+			$attributs = "";
+			for ($i=0; $i < nbAttribut; $i++) { 
+				$attributs = $attributs."\t\t".$droit.$tabAttribut[i]."\n"
+			}
+			//Création du texte formaté
+			$texte = $phpStart.$include.$classHeader.$attributs.$classFooter.$phpEnd;
+			//Création du fichier de la classe
+			$manip = fopen($className.".class.php", "w+");
+			if($manip==false)
+				die("fuck");
+			fputs($manip, $texte);
+		}
+	}
+?>
