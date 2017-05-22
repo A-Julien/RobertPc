7d764bb137afa4de36f35414b0c0525c61dbde76
EYMERY Pierre <eymeryp@hostname.iut2.upmf-grenoble.fr>
2017-05-22 12:54:46 +0200

    Mise en place mvc

---
+++ vue/accueil.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" type="text/css" media="screen" href="accueil.css"/>
+    <title>Accueil RobertPC</title>
+  </head>
+  <body>
+    <header>
+      Coucou
+    </header>
+
+    <nav id="recherche">
+      Coucou
+    </nav>
+
+    <div id="containerListeCat">
+      <ul id="listeCat">
+        Coucou
+      </ul>
+    </div>
+
+    <div id="whiteBox">
+      Coucou
+    </div>
+
+    <footer>
+      Coucou
+    </footer>
+
+  </body>
+</html>
