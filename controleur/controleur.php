03aa54c27b51415c6f1bc8b1e7feb74489718219
EYMERY Pierre <eymeryp@hostname.iut2.upmf-grenoble.fr>
2017-05-24 13:21:11 +0200

    maj ctrleur

--- controleur/controleur.php
+++ controleur/controleur.php
@@ -20,14 +20,14 @@
 
 	$action();
 
-	function getCat() {
+	function getCat() { //ouvre la page d'accueil
 		global $robert;
 		global $data;
 		$data = $robert->getCategories();
 		include("../vue/accueil.php");
 	}
 
-	function getListe() {
+	function getListe() { //ouvre la page web "liste des produits par catégorie"
 		global $categorie;
 		global $robert;
 		global $tabListe;
@@ -38,7 +38,7 @@
 		include("../vue/liste.php");
 	}
 
-	function getObj() {
+	function getObj() { //ouvre la page web "produit"
 		global $categorie;
 		global $robert;
 		global $data;
@@ -51,7 +51,7 @@
 		include("../vue/produit.php");
 	}
 
-	function ajouterCat() {
+	function ajouterCat() { //ouvre le formulaire permettant de déterminer les noms et types des attributs de la nouvelle catégorie
 		include("../vue/ajoutCat.php");
 	}
 
@@ -62,24 +62,44 @@
 		$tabCreaCat = array();
 		for ($i=1; isset($_POST['nomAttr'.$i]) ; $i++) {
 			if(isset($_POST['nomAttr'.$i])){
-				$tabCreaCat[$_POST['nomAttr'.$i]] = $_POST['typeAttr'.$i];
+				$tabCreaCat[$i] = $_POST['nomAttr'.$i];
 			}
 		}
 		include("../vue/ajoutCatConfirm.php");
 	}
 
-	function choixProduit() {
+	function choixProduit() { //ouvre le formulaire permettant de choisir le type de produit à ajouter
 		include("../vue/formulaireAdd.html");
 	}
 
-	function add() {
+	function add() { //ouvre le formulaire pour renseigner les valeurs des attributs du nouveau produit
 		global $categorie;
 		$categorie = $_POST['categorie'];
 		include("../vue/add.php");
 	}
 
-	function ajouterProduit() {
-		
+	function ajouterProduit() { //effectue l'ajout dans la base de données
+
+	}
+
+	function supprimerProduit() { //ouvre le formulaire permettant de choisir le type de produit à ajouter
+		global $robert;
+		global $data;
+		$data = $robert->getCategories();
+		include("../vue/formulaireSuppr.php");
+	}
+
+	function suppr() { //ouvre le formulaire permettant de choisir le produit à supprimer dans la liste des produits de sa catégorie
+		global $categorie;
+		global $robert;
+		global $tabListe;
+		$categorie = $_POST['categorie'];
+		$tabListe = $robert->getListe($categorie);
+		include("../vue/suppr.php");
+	}
+
+	function supprObj() { //effectue la suppression de l'objet sélectionné dans la base de données
+
 	}
 
 ?>
--- controleur/fonctionsAjoutCat.js
+++ controleur/fonctionsAjoutCat.js
@@ -2,7 +2,7 @@ var numAttr = 0;
 
 function ajout() {
   numAttr ++;
-  
+
   var zoneAttr = document.getElementById('zoneAttr');
 
   var nomAttr = document.createElement('input');
@@ -12,14 +12,6 @@ function ajout() {
   nomAttr.setAttribute('placeholder', 'Nom de l\'attribut');
   nomAttr.setAttribute('value', '');
 
-  var typeAttr = document.createElement('input');
-  typeAttr.setAttribute('type', 'text');
-  typeAttr.setAttribute('name', 'typeAttr' + numAttr );
-  typeAttr.setAttribute('id', 'typeAttr' + numAttr);
-  typeAttr.setAttribute('placeholder', 'Type de l\'attribut');
-  typeAttr.setAttribute('value', '');
-
   zoneAttr.appendChild(document.createElement('br'));
   zoneAttr.appendChild(nomAttr);
-  zoneAttr.appendChild(typeAttr);
 }
